8e9af368cd00d1f9298f09f54ad8b4203c788f37
Vladislav Makhov <vladislav.makhov@stm-labs.ru>
2022-05-27 21:01:07 +0300

    fix

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -59,19 +59,21 @@ const renderCurrentFileList = (currentFileList: any[] = [], setCurrentFiles: any
                             e.stopPropagation();
                             const downloadUrl = new UrlBuilder().build(DOWNLOAD_FILE, file?.id).url;
 
-                            // api.post(downloadUrl).then((response: any) => {
-                            //     const url = window.URL.createObjectURL(new Blob([response]));
-                            //     const link = document.createElement('a');
-                            //     link.href = url;
-                            //     link.setAttribute('download', file.name);
-                            //     document.body.appendChild(link);
-                            //     link.click();
-                            //     link.remove();
-                            //
-                            // });
-                            api.post(downloadUrl, { responseType: 'blob' })
-                                .then((response: any) => response.blob())
-                                .then((blob) => saveAs(blob, file.name));
+                            api.post(downloadUrl, undefined, { responseType: 'blob' }).then(
+                                (response: any) => {
+                                    const url = window.URL.createObjectURL(new Blob([response]));
+                                    const link = document.createElement('a');
+                                    link.href = url;
+                                    link.setAttribute('download', file.name);
+                                    document.body.appendChild(link);
+                                    link.click();
+                                    link.remove();
+                                },
+                            );
+
+                            // api.post(downloadUrl, { responseType: 'blob' })
+                            //     .then((response: any) => response.blob())
+                            //     .then((blob) => saveAs(blob, file.name));
                         }}
                     >
                         {file.name}

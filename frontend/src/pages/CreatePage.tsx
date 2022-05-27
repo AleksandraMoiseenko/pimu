c3ddfb62b21357dc1aec05a67e8a7f4b1d2a1499
Vladislav Makhov <vladislav.makhov@stm-labs.ru>
2022-05-27 21:22:03 +0300

    fix

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -58,27 +58,7 @@ const renderCurrentFileList = (currentFileList: any[] = [], setCurrentFiles: any
                         onClick={(e) => {
                             e.stopPropagation();
                             const downloadUrl = new UrlBuilder().build(DOWNLOAD_FILE, file?.id).url;
-
-                            // api.post(downloadUrl, undefined, { responseType: 'blob' }).then(
-                            //     (response: any) => {
-                            //         const url = window.URL.createObjectURL(new Blob([response]));
-                            //         const link = document.createElement('a');
-                            //         link.href = url;
-                            //         link.setAttribute('download', file.name);
-                            //         document.body.appendChild(link);
-                            //         link.click();
-                            //         link.remove();
-                            //     },
-                            // );
-
-                            api.post(downloadUrl, undefined, { responseType: 'blob' }).then(
-                                (response: any) =>
-                                    saveAs(
-                                        [response],
-                                        { type: 'application/octet-stream' },
-                                        file.name,
-                                    ),
-                            );
+                            saveAs(`${baseURL}${downloadUrl}`, file.name);
                         }}
                     >
                         {file.name}
